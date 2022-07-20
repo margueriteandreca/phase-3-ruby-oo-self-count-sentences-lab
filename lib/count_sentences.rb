8208b971ab48ec0dfad6d8cf48dc5c4671a54d48
+ <+>
2022-07-20 01:01:01 -0400

    commit tree f15a558feefd2fde66705cdcb0d2df5e8d1e57f4

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,14 +3,16 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
+    self.end_with?("?") ? true : false
 
   end
 
   def exclamation?
+    self.end_with?("!") ? true : false
 
   end
 
